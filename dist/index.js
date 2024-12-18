56f73521bd78d91eea9008dfa8ce53e1b030ac24
Şükrü Taha Bıyık <137053406+tahab-idealdata@users.noreply.github.com>
2024-12-18 12:08:28 +0300

    ...

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/FonService.ts
-const hello_service_1 = require("./services/hello.service");
+const hello_service_1 = __importDefault(require("./services/hello.service"));
 class FonCore {
     static initialize(apiUrl, token) {
         this.apiUrl = apiUrl;

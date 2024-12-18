c6ca9155d76d87c2f19b0d467d54cd116b853bcc
Şükrü Taha Bıyık <137053406+tahab-idealdata@users.noreply.github.com>
2024-12-18 14:06:22 +0300

    ...

---
+++ dist/esm/services/fund.service.d.ts
@@ -0,0 +1,10 @@
+declare class FundService {
+    private static instance;
+    static configure({ apiUrl, token }: {
+        apiUrl: string;
+        token: string;
+    }): void;
+    static getFunds(): Promise<any>;
+}
+export default FundService;
+//# sourceMappingURL=fund.service.d.ts.map
